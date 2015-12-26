a5f837c8b78320011beb22d91726408a3ca00862
Duckzbug <domduxbury@hotmail.com>
2015-12-26 13:13:29 +0000

    Initial Commit

---
+++ src/app/main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HomePage from '../containers/homePage.js';
+import Bootstrap from 'bootstrap';
+
+let appNode = document.getElementById('app');
+ReactDOM.render(<HomePage/>, appNode);
